494268e479bca4c4b9435b4bc14c81eaf1fa0431
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-17 11:42:54 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-teams.php");
 
+$pageTitle="Teams";
+include "view-header.php";
+$teams = selectTeams();
+include "view-teams.php";
+include "view-footer.php";
+?>
