e12de21deabc4b368767fda21d8b962682f8bdcf
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-17 12:04:44 -0500

    Create teams-by-player.php

---
+++ teams-by-player.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-teams-by-player.php");
+
+$pageTitle="Teams by Player";
+include "view-header.php";
+$teams = selectTeamsByPlayer($_GET['id']);
+include "view-teams-by-player.php";
+include "view-footer.php";
+?>
