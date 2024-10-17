4e17abf255a86153190c7939ca1be94b43849739
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-17 12:33:55 -0500

    Create view-teams-by-player.php

---
+++ view-teams-by-player.php
@@ -0,0 +1,29 @@
+<h1>Teams by Player</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+       <th>ID</th>
+       <th>Team Name</th>
+       <th>Division</th>
+        <th>Points Per Game</th>
+        <th>Postion Rank</th>
+      </tr>
+    </thead>
+    <tbody>   
+  <?php
+    while ($team = $teams->fetch_assoc()) {
+      ?>
+      <tr>
+        <td><?php echo $team['team_id']; ?></td>
+         <td><?php echo $team['team_name']; ?></td>
+        <td><?php echo $team['team_division']; ?></td>
+         <td><?php echo $team['avg_points']; ?></td>
+          <td><?php echo $team['postion_rank']; ?></td>
+      </tr>
+      <?php
+    }
+   ?>
+      </tbody>
+  </table>
+</div>
