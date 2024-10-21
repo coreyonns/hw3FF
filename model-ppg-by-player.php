2830b5eee69c5290de54939c4ca55753f30d8028
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-21 13:15:41 -0500

    Update model-ppg-by-player.php

--- model-ppg-by-player.php
+++ model-ppg-by-player.php
@@ -2,9 +2,9 @@
 function selectPpgByPlayer($pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT t.team_id, team_name, team_division, avg_points, position_rank
-FROM `team` t join ranking r on r.team_id= t.team_id where r.player_id=?");
-       $stmt->bind_param("i", $iid);
+        $stmt = $conn->prepare("SELECT t.team_id, team_name, team_division, avg_points, position_rank FROM `team` t join ranking r on r.team_id= t.team_id 
+        where r.team_id=?");
+       $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
