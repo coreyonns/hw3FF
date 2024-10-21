80dc8099973ac3beadd04f70780d3618e9eb3b79
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-21 13:31:31 -0500

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -3,10 +3,11 @@
   <table class="table">
     <thead>
       <tr>
-       <th>ID</th>
-       <th>Name</th>
-       <th>Position</th>
-        <th></th>
+           <th>ID</th>
+           <th>Name</th>
+           <th>Position</th>
+            <th></th>
+            <th></th>
       </tr>
     </thead>
     <tbody>   
@@ -14,10 +15,16 @@
     while ($player = $players->fetch_assoc()) {
       ?>
       <tr>
-        <td><?php echo $player['player_id']; ?></td>
-         <td><?php echo $player['player_name']; ?></td>
-        <td><?php echo $player['player_position']; ?></td>
-         <td><a href="teams-by-player.php?id=<?php echo $player['player_id']; ?>">Teams</a></td>
+            <td><?php echo $player['player_id']; ?></td>
+             <td><?php echo $player['player_name']; ?></td>
+            <td><?php echo $player['player_position']; ?></td>
+             <td><a href="teams-by-player.php?id=<?php echo $player['player_id']; ?>">Teams</a></td>
+            <td>
+              <form method= "post" action="ppg-by-player.php">
+                <input type = "hidden" name= "pid" value= "<?php echo $player['player_id']; ?>">
+                <button type="submit" class="btn btn-primary">PPG</button>
+              </form>
+        </td>
       </tr>
       <?php
     }
