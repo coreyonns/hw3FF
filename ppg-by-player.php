62312f0c4e602b7888414658b15c2e8b74444656
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-21 13:07:43 -0500

    Create ppg-by-player.php

---
+++ ppg-by-player.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-ppg-by-player.php");
+
+$pageTitle="PPG by Player";
+include "view-header.php";
+$ppg = selectPpgByPlayer($_POST['cid']);
+include "view-ppg-by-player.php";
+include "view-footer.php";
+?>
