3830d33d7942641f2336d45be6525969fca833cb
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-21 14:02:26 -0500

    Update view-teams-by-player.php

--- view-teams-by-player.php
+++ view-teams-by-player.php
@@ -6,8 +6,7 @@
        <th>ID</th>
        <th>Team Name</th>
        <th>Division</th>
-        <th>Points Per Game</th>
-        <th>Postion Rank</th>
+        
       </tr>
     </thead>
     <tbody>   
@@ -18,8 +17,7 @@
         <td><?php echo $team['team_id']; ?></td>
          <td><?php echo $team['team_name']; ?></td>
         <td><?php echo $team['team_division']; ?></td>
-         <td><?php echo $team['avg_points']; ?></td>
-          <td><?php echo $team['position_rank']; ?></td>
+        
       </tr>
       <?php
     }
