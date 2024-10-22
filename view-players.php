228025b4bea6bf21903984edcc767978a11aead8
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-22 09:33:13 -0500

    Update view-players.php

--- view-players.php
+++ view-players.php
@@ -7,7 +7,6 @@
            <th>Name</th>
            <th>Position</th>
             <th></th>
-            <th></th>
       </tr>
     </thead>
     <tbody>   
@@ -19,12 +18,7 @@
              <td><?php echo $player['player_name']; ?></td>
             <td><?php echo $player['player_position']; ?></td>
              <td><a href="teams-by-player.php?id=<?php echo $player['player_id']; ?>">Teams</a></td>
-            <td>
-              <form method= "post" action="ppg-by-player.php">
-                <input type = "hidden" name= "pid" value= "<?php echo $player['player_id']; ?>">
-                <button type="submit" class="btn btn-primary">PPG</button>
-              </form>
-        </td>
+           
       </tr>
       <?php
     }
