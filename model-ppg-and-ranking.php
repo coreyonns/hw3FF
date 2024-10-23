938777a13a0e5bacc3320040c0c298f2c39fbe33
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-23 13:26:11 -0500

    Update model-ppg-and-ranking.php

--- model-ppg-and-ranking.php
+++ model-ppg-and-ranking.php
@@ -1,10 +1,10 @@
 <?php
-function selectPPGandRanking($iid) {
+function selectPPGandRanking() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT p.player_id, player_name, player_position, avg_points,position_rank
  FROM `player` p join ranking r on  r.player_id=p.player_id where r.player_id=?");
-        $stmt->execute("i",$iid);
+        $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
         return $result;
