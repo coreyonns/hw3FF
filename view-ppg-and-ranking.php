766ca597fd870d7811055009bf492f4b6bd6fba0
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-28 13:12:34 -0500

    Update view-ppg-and-ranking.php

--- view-ppg-and-ranking.php
+++ view-ppg-and-ranking.php
@@ -15,9 +15,9 @@
     while ($ppg = $ppgs->fetch_assoc()) {
       ?>
       <tr>
-        <td><?php echo $ppg['team_id']; ?></td>
-         <td><?php echo $ppg['team_name']; ?></td>
-        <td><?php echo $ppg['team_division']; ?></td>
+        <td><?php echo $ppg['player_id']; ?></td>
+         <td><?php echo $ppg['player_name']; ?></td>
+        <td><?php echo $ppg['player_position']; ?></td>
           <td><?php echo $ppg['avg_points']; ?></td>
             <td><?php echo $ppg['position_rank']; ?></td>
 
