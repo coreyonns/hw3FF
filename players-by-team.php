5a786f9675f20655ebfcbf93768c4692243ab1c9
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-28 13:35:38 -0500

    Update and rename players-by-team to players-by-team.php

--- players-by-team.php
+++ players-by-team.php
@@ -4,7 +4,7 @@ require_once("model-players-by-team.php");
 
 $pageTitle="Players by Team";
 include "view-header.php";
-$playerteam = selectPlayersByTeam($_POST['pid']);
+$playerteams = selectPlayersByTeam($_POST['pid']);
 include "view-players-by-team.php";
 include "view-footer.php";
 ?>
