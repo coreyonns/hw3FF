293c078c41a6b24f28dac483c9651da7b368cd2d
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-28 13:33:48 -0500

    Create model-players-by-team.php

---
+++ model-players-by-team.php
@@ -0,0 +1,17 @@
+<?php
+function selectPlayersByTeam($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT p.player_id, player_name, player_positionFROM `player` p join ranking r on r.player_id= p.player_id
+where r.player_id=?");
+       $stmt->bind_param("i", $pid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
