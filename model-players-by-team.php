c1c632e26e7c1ab1fd21608159690ba338183aff
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-10-28 13:45:58 -0500

    Update model-players-by-team.php

--- model-players-by-team.php
+++ model-players-by-team.php
@@ -2,8 +2,8 @@
 function selectPlayersByTeam($pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT p.player_id, player_name, player_positionFROM `player` p join ranking r on r.player_id= p.player_id
-where r.player_id=?");
+        $stmt = $conn->prepare("SELECT p.player_id, player_name, player_position FROM `player` p join ranking r on r.player_id= p.player_id
+where r.player_id=?);
        $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
