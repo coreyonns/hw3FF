10079d9441dea894e5c920aa6eef061503108b70
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-11-05 12:45:18 -0600

    Create view-information.php

---
+++ view-information.php
@@ -0,0 +1,28 @@
+<h1>Information</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+           <th>Name</th>
+           <th>Total Yards Last Game</th>
+           <th>Total Touches Last Game</th>
+            <th>Total Touchdowns Last Game</th>
+      </tr>
+    </thead>
+    <tbody>   
+  <?php
+    while ($infos = $info->fetch_assoc()) {
+      ?>
+      <tr>
+            <td><?php echo $infos['player_name']; ?></td>
+             <td><?php echo $infos['yards']; ?></td>
+            <td><?php echo $infos['touches']; ?></td>
+             <td><?php echo $infos['touchdowns']; ?></td>
+           
+      </tr>
+      <?php
+    }
+   ?>
+      </tbody>
+  </table>
+</div>
