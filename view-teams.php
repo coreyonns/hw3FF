b4132c8dc3e6df23d8ec589a1aa347c1c8f2c096
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-11-11 13:55:06 -0600

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -31,8 +31,9 @@ include "view-teams-newform.php";
         <td>
         </td>
         <td>
-          <form method="post" action="players-by-team.php">
+          <form method="post" action="">
             <input type="hidden" name="pid" value="<?php echo $team['team_id']; ?>">
+            <input type="hidden" name="actionType" value="Delete">
             <button type="submit" class="btn btn-primary">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
