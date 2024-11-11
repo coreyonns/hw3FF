a21f8539772ad6f2083fa01b410dbfe16510dc5c
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-11-11 13:34:42 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -8,7 +8,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']) {
     case "Add":
-      if(insertTeam($_POST['tName'],$_POST['tDiv'])); {
+      if(insertTeam($_POST['tName'], $_POST['tDiv'])) {
         echo '<div class="alert alert-success" role="alert">Team added!</div>"';
         } else {
           echo '<div class="alert alert-danger" role="alert">Error!</div>"';
