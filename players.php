c72a22272cb367c35176e938ee65ea9a8148ccdb
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-11-18 13:43:19 -0600

    Update players.php

--- players.php
+++ players.php
@@ -4,6 +4,32 @@ require_once("model-players.php");
 
 $pageTitle="Players";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']) {
+    case "Add":
+      if(insertPlayers($_POST['pName'], $_POST['pPosition'])) {
+        echo '<div class="alert alert-success" role="alert">Player added!</div>';
+        } else {
+          echo '<div class="alert alert-danger" role="alert">Error!</div>';
+        }
+    break;
+     case "Delete":
+      if(deletePlayers($_POST['pid'])) {
+        echo '<div class="alert alert-success" role="alert">Player deleted!</div>';
+        } else {
+          echo '<div class="alert alert-danger" role="alert">Error!</div>';
+        }
+    break;
+     case "Edit":
+      if(updatePlayers($_POST['pName'], $_POST['pPosition'],$_POST['pid'])) {
+        echo '<div class="alert alert-success" role="alert">Player edited!</div>';
+        } else {
+          echo '<div class="alert alert-danger" role="alert">Error!</div>';
+        }
+    break;
+  }
+}
 $players = selectPlayers();
 include "view-players.php";
 include "view-footer.php";
