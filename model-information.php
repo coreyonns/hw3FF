fe84e84e32d84c6d9aa92d762dbd5301c70019cb
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-11-18 15:35:42 -0600

    Update model-information.php

--- model-information.php
+++ model-information.php
@@ -13,4 +13,44 @@ FROM `information`");
         throw $e;
     }
 }
+
+function insertInfo($pName,$pYards,$pTouches,$pTouchdowns) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `information` `player_name`, `yards`, `touches`, `touchdowns` VALUES (?, ?, ?, ?))";
+         $stmt->bind_param("ss", $pName,$pYards,$pTouches,$pTouchdowns);
+       $success= $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateInfo($pName,$pPosition,$pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `player` set `player_name`=?, `player_position`=? where player_id=?");
+         $stmt->bind_param("ssi", $pName,$pPosition,$pid);
+       $success= $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteInfo($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from player where player_id=?");
+         $stmt->bind_param("i", $pid);
+       $success= $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
