98ccac10db96acc621742badae8c5ec9022a4872
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-12-02 14:32:55 -0600

    Update view-information.php

--- view-information.php
+++ view-information.php
@@ -12,6 +12,7 @@ include "view-information-newform.php";
   <table class="table">
     <thead>
       <tr>
+           <th>Information ID</th>
            <th>Name</th>
            <th>Total Yards Last Game</th>
            <th>Total Touches Last Game</th>
@@ -25,7 +26,8 @@ include "view-information-newform.php";
     while ($infos = $info->fetch_assoc()) {
       ?>
       <tr>
-            <td><?php echo $infos['player_name']; ?></td>
+          <td><?php echo $infos['information_id']; ?></td>
+          <td><?php echo $infos['player_name']; ?></td>
              <td><?php echo $infos['yards']; ?></td>
             <td><?php echo $infos['touches']; ?></td>
              <td><?php echo $infos['touchdowns']; ?></td>
@@ -33,7 +35,16 @@ include "view-information-newform.php";
              
            </td>
         <td>
-          
+           <form method="post" action="">
+            <input type="hidden" name="iid" value="<?php echo $infos['information_id']; ?>">
+            <input type="hidden" name="actionType" value="Delete">
+            <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+            </svg>
+            </button>
+           </form>  
         </td>
       </tr>
       <?php
