0faac7618ac87cd807467ecaa4348ae2e90601cd
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-12-02 13:47:17 -0600

    Update model-information.php

--- model-information.php
+++ model-information.php
@@ -13,4 +13,43 @@ FROM `information`");
         throw $e;
     }
 }
+function insertInfo($iPlayer,$iYards,$iTouches,$iTouchdowns) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `information` (`player_name`, `yards`, `touches`, `touchdowns`) VALUES (?,?,?,?);");
+          $stmt->bind_param("siii",$iPlayer,$iYards,$iTouches,$iTouchdowns);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateInfo($iPlayer,$iYards,$iTouches,$iTouchdowns,$iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `information` set `player_name`=?, `yards`=?, `touches`=?, `touchdowns`=? where information_id=?");
+          $stmt->bind_param("siiii",$iPlayer,$iYards,$iTouches,$iTouchdowns,$iid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteInfo($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from information where information_id=?");
+          $stmt->bind_param("i",$iid);
+        $success=$stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
