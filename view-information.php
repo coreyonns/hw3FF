8cdf38104e3499b09526949ff9016fc7a290f3de
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-12-02 13:50:49 -0600

    Update view-information.php

--- view-information.php
+++ view-information.php
@@ -1,4 +1,13 @@
-<h1>Information</h1>
+<div class = "row">
+<div class = "col">
+  <h1>Information</h1>
+  </div>
+  <div class ="col-auto">
+<?php
+include "view-information-newform.php";
+?>
+</div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
