71620d7661403c34bcc3e8f9c727cd255d8eec43
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-12-03 12:45:06 -0600

    Create view-team-input-list.php

---
+++ view-team-input-list.php
@@ -0,0 +1,9 @@
+<select class="form-select" id="tName" name="tName">
+<?php 
+while ($teamItem= $teamList->fetch_assoc()){
+?>
+    <option value="<?php echo $teamItem ['team_id'];?>"><?php echo $teamItem ['team_name'];?></option>
+  <?php
+}
+?>  
+</select>
