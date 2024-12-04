3ece29e27976f5c27a95a80710e2d58f0c8248af
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-12-03 19:40:49 -0600

    Update view-teams-input-list.php

--- view-teams-input-list.php
+++ view-teams-input-list.php
@@ -1,9 +1,35 @@
-<select class="form-select" id="tName" name="tName">
-<?php 
-while ($teamItem= $teamList->fetch_assoc()){
-?>
-    <option value="<?php echo $teamItem ['team_id'];?>"><?php echo $teamItem ['team_name'];?></option>
-  <?php
-}
-?>  
+<select class="form-select" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">Washhngton Commanders</option>
+  <option value="2">New York Jets</option>
+  <option value="3">Detroit Lions</option>
+    <option value="4">Baltimore Ravens</option>
+  <option value="5">Arizona Cardinals</option>
+  <option value="6">Los Angeles Rams</option>
+    <option value="7">Kansas City Chiefs</option>
+  <option value="8">Atlanta Falcons</option>
+  <option value="9">Carolina Panthers</option>
+    <option value="10">Chicago Bears</option>
+  <option value="11">Dallas Cowboys</option>
+  <option value="12">Green Bay Packers</option>
+    <option value="13">Minnesota Vikings</option>
+  <option value="14">Two</option>
+  <option value="15">Three</option>
+    <option value="15">One</option>
+  <option value="17">Two</option>
+  <option value="18">Three</option>
+    <option value="19">One</option>
+  <option value="20">Two</option>
+  <option value="21">Three</option>
+    <option value="22">One</option>
+  <option value="23">Two</option>
+  <option value="24">Three</option>
+    <option value="25">One</option>
+  <option value="26">Two</option>
+  <option value="27">Three</option>
+    <option value="28">One</option>
+  <option value="29">Two</option>
+  <option value="30">Three</option>
+    <option value="31">One</option>
+  <option value="32">Two</option>
 </select>
