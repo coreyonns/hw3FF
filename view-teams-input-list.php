f56124a816d2f456df74c06402f71ce8e9ef1aa1
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-12-03 19:50:33 -0600

    Update view-teams-input-list.php

--- view-teams-input-list.php
+++ view-teams-input-list.php
@@ -2,7 +2,7 @@
 <?php 
 while ($teamItem= $teamList->fetch_assoc()){
 ?>
-    <option value="<?php echo $teamItem ['team_id'];?>"><?php echo $teamItem ['team_name'];?></option>
+    <option value="<?php echo $teamItem ['team_name'];?>"><?php echo $teamItem ['team_name'];?></option>
   <?php
 }
 ?>  
