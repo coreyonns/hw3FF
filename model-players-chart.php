8af7849bcc57b852e92c771c7889ee95fe4f398a
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-12-12 18:46:59 -0600

    Update model-players-chart.php

--- model-players-chart.php
+++ model-players-chart.php
@@ -1,5 +1,16 @@
 <?php
-function selectInstructors(){
-  try{
-    $conn =get_db_connection();
-    $stmt = $conn->prepare("SELECT player_name, count(
+function selectPlayers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT p.player_name,MAX(i.yards) as num_yards
+FROM `player` p join information i on i.player_id=p.player_id GROUP by p.player_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
