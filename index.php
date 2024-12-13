3c03b622b3173d41b52c757fffe3f8d811951e5b
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-12-13 08:49:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,18 @@ include('view-header.php');
 ?>
 <h1>Welcome to Fantasy Football</h1>
 <p>Your one-stop hub for managing your teams, players, and rankings.</p>
+  <!-- Example Card -->
+    <div class="container mt-5">
+      <div class="card">
+        <div class="card-header">
+          Player Stats
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Top Players</h5>
+          <p class="card-text">Check out how player have been performing on your team!</p>     
+        </div>
+      </div>
+    </div>
 </div> 
 </body>
 </html>
