ad25b7a5c9af48cfac9a8c016a51a76f97131051
coreyonns <coreyonn.c.stokes-1@ou.edu>
2024-12-12 20:10:12 -0600

    Update positions-chart.php

--- positions-chart.php
+++ positions-chart.php
@@ -5,6 +5,6 @@ require_once("model-positions-chart.php");
 $pageTitle="Positions Count";
 include "view-header.php";
 $players = selectPlayers();
-include "view-postions-chart.php";
+include "view-positions-chart.php";
 include "view-footer.php";
 ?>
